7db7668e07bcfd561e1350a1b9ce667b7daa864f
ichanddan <code.us.cm@gmail.com>
2024-04-22 01:14:01 +0530

    modify ui

--- src/components/Convertor.jsx
+++ src/components/Convertor.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 export const Convertor = () => {
   const [value, setValu] = useState(1);
+  document.title="Currency Convertor"
   return (
     <div className="flex items-center justify-center mt-10">
       <div className="w-1/2 border p-6 rounded-2xl flex items-center justify-center">
@@ -35,6 +36,7 @@ export const Convertor = () => {
               <option value="volvo">use</option>
             </select>
           </div>
+            <button className="p-1.5 bg-fuchsia-600 w-11/12 rounded-md mt-3 text-white">Convert</button>
           <h1 className="text-center mt-10 text-6xl">{value}</h1>
         </div>
       </div>
